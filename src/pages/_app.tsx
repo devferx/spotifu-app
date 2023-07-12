ee54456ca3aeffd239de90c3c77a35fe3a649240
devferx <ferkki@hotmail.com>
2023-07-12 16:12:02 -0400

    :heavy_plus_sign: [ADD] React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,31 @@
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { AuthProvider } from "@/context/AuthContext";
+import { PlayerProvider } from "@/context/PlayerContext";
 import { SpotifyProvider } from "@/context/SpotifyContext";
 
 import { Layout } from "@/ui/Layout";
 
 import "@/styles/globals.css";
 import "react-loading-skeleton/dist/skeleton.css";
-import { PlayerProvider } from "@/context/PlayerContext";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <SpotifyProvider>
-        <PlayerProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </PlayerProvider>
-      </SpotifyProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <SpotifyProvider>
+          <PlayerProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </PlayerProvider>
+        </SpotifyProvider>
+      </AuthProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
